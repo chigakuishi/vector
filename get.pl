5557bb77e1c6fffdc5834fbed869da39d011b96b
Rhenium <chigakuishi.soft@gmail.com>
2017-06-11 23:00:50 +0900

    dl multi files

--- get.pl
+++ get.pl
@@ -9,30 +9,33 @@ use Encode;
 our $dlpath = "/tmp/vector/";
 our $host = "http://www.vector.co.jp";
 #my  $url = "/vpack/filearea/winnt/hardware/av";
-my $url=<STDIN>;
-chomp $url;
-$url=$1 if($url=~/https?:\/\/[^\/]+(\/.*)$/);
-sub dlpage{
-  my $path = shift;
-  my $name = shift;
-  my $source = decode("sjis", `wget -O - '$host$path' 2>/dev/null`);
-  if($source=~/<a\s+href="(http:\/\/ftp.vector.co.jp[^"]+)"/i){
-    my $dlurl = $1;
-    print "wget -P $dlpath '$dlurl'\n";
-    `wget -P $dlpath '$dlurl'`;
+while(<STDIN>){
+  my $url;
+  chomp;
+  $url = $1 if(/https?:\/\/[^\/]+(\/.*)$/);
+  $url = $_ unless(/https?:\/\/[^\/]+(\/.*)$/);
+  sub dlpage{
+    my $path = shift;
+    my $name = shift;
+    my $source = decode("sjis", `wget -O - '$host$path' 2>/dev/null`);
+    if($source=~/<a\s+href="(http:\/\/ftp.vector.co.jp[^"]+)"/i){
+      my $dlurl = $1;
+      print "wget -P $dlpath '$dlurl'\n";
+      `wget -P $dlpath '$dlurl'`;
+    }
   }
-}
-sub detailpage{
-  my $path = shift;
-  my $name = shift;
-  $path =~ s/\/soft/\/soft\/dl/g;
-  my $source=decode("sjis", `wget -O - '$host$path' 2>/dev/null`);
-  if($source=~/<a\s+href="(\/download\/[^"]+)"/i){
-    &dlpage($1, $name);
+  sub detailpage{
+    my $path = shift;
+    my $name = shift;
+    $path =~ s/\/soft/\/soft\/dl/g;
+    my $source=decode("sjis", `wget -O - '$host$path' 2>/dev/null`);
+    if($source=~/<a\s+href="(\/download\/[^"]+)"/i){
+      &dlpage($1, $name);
+    }
   }
-}
 
-my $source = decode("sjis", `wget -O - '$host$url' 2>/dev/null`);
-while($source =~ /<A\s+HREF="(\/soft[^"]+)"\s*>([^<]+)<\/A>/gi){
-  &detailpage($1, $2);
+  my $source = decode("sjis", `wget -O - '$host$url' 2>/dev/null`);
+  while($source =~ /<A\s+HREF="(\/soft[^"]+)"\s*>([^<]+)<\/A>/gi){
+    &detailpage($1, $2);
+  }
 }
